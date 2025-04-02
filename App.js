f4858310a3a5f44572d2d672e1c38f8b5ffb48ff
animeshjaiswalpratishthan <animesh.jaiswal@pratishthanventures.com>
2025-04-02 17:03:16 +0530

    Episode-04

--- App.js
+++ App.js
@@ -1,54 +1,111 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Just like DOM element in Javascript
-// We have react element in React
+const Header = () =>{
+    //header will have logo and nav item
+    return(
+        <div className="header">
 
-const heading = React.createElement("h1",{id:"heading"},"Namaste React 🚀");
-// Now its an object not html element
-console.log("heading", heading)
+            {/* logo div */}
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"/>
 
-//This is how we can create h1 element using JSX
-const jsxHeading = <h1 id="heading"> Namaste react using JSX 🚀</h1>
-console.log("jsx heading",jsxHeading)
+            </div>
 
-//when we render this element to the DOM it becomes the html.
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// whatever will happen inside this root will be rendered to the DOM
-
-//React functional component
-//Its a normal javascript function which returns some JSX element
-// const HeadingComponent = () => (
-//     <div id="container">
-//          <h1 className="heading">Namaste React Functional Component</h1>
-//     </div>
-// )
-
-
-const TitleComponent = () =>(
-    <h1 className="head">
-        Namaste React using JSX
-    </h1>
-);
-
-const number = 100000;
+            {/* Nav item div */}
+            <div className="nav-items">
+                {/* Nac item is nothing but a collection of list */}
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
 
-//suppose if we want to render this title component , inside our Heading Component.
-//this is also called as Component Composition.
-const HeadingComponent = () => (
-    <div id="container">
-        {jsxHeading}
-         <h1 className="heading">Namaste React Functional Component</h1>
+// whenever we want to give inline css, we can give it via creating a js object.
+
+
+
+const RestaurantCard = (props) =>{ 
+
+    const {resData} = props;
+
+    const {
+        name,
+        avgRating,
+        cuisines,
+        costForTwo,
+        deliveryTime
+    } = resData?.data;
+
+    console.log(props)
+    return (
+        <div className="res-card" style={{
+            backgroundColor: "f0f0f0",
+        }}> 
+        <img 
+        className="res-logo"
+        alt="res-logo" 
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZBzRlrQuxVDoz_eyWvpQ12N8vSQcpkKxQ8Q&s" />
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{costForTwo/100} For Two</h4>
+        <h4>{deliveryTime} minutes</h4>
+        </div>  
+    )
+}
+
+//this resObj is a javascript object
+const resObj = {
+    data:{
+        name:"KFC",
+        costForTwo: 40000,
+        costForTwoString: "400 FOR TWO",
+        avgRating:"3.8",
+        cuisines:["Burgers","Biryani","American","Snacks","Fast Food"],
+        deliveryTime:36
+    }
+}
+
+
+const Body = () =>{
+    return (
+        <div className="body">
+            {/* search bar */}
+            <div className="search">Search</div>
+
+            {/* Restaurant Container */}
+            <div className="rest-container">
+                {/*  it will contain lot of restaurant cards */}
+                <RestaurantCard 
+                    resData={resObj}
+                />
+                
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () =>{
+    return (
+    <div className="app">
+       <Header/>
+       <Body/>
+
     </div>
-)
+    )
+}
 
 
-// root.render(jsxHeading);
-//it will replace whatever is present inside the root.
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// if we want to render our functional component , how we can render it.
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
 
 
 
